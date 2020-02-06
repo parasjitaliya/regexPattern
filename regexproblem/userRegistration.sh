5377d5a35e6df3f13b1137735d577bf8e6dfe3f9
paras <parasjitaliya@gmail.com>
2020-02-06 16:35:15 +0530

    Added:lastnamevalidation

--- regexproblem/userRegistration.sh
+++ regexproblem/userRegistration.sh
@@ -8,3 +8,12 @@ else
    echo "FirstName  $firstName is invalid "
 	echo "enter firstname with minimum 3 characters.."
 fi
+read -p "enter last name:" lastName
+if [[ "$lastName" =~ ^[A-Za-Z]{3,}$ ]]
+then
+   echo "lastName  $lastName is valid."
+else
+   echo "laststName  $lastName is invalid "
+        echo "enter lastname with minimum 3 characters.."
+fi
+
