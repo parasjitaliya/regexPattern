046653338fec7b5ac0de9d994c70215318941364
paras <parasjitaliya@gmail.com>
2020-02-06 15:36:49 +0530

    validation for firstname

---
+++ regexproblem/userRegistration.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+read -p "enter first name:" firstName
+if [[ "$firstName" =~ ^[A-Za-Z]{3,}$ ]]
+then
+   echo "firstName  $firstName is valid."
+else
+   echo "FirstName  $firstName is invalid "
+	echo "enter firstname with minimum 3 characters.."
+fi
